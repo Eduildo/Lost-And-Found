fb97aac0e3d6a4149cef056caba609419cc09ca8
eduildo <51817346+Eduildo@users.noreply.github.com>
2023-06-13 17:23:16 +0100

    item card container

--- frontend/src/components/screens/Home.js
+++ frontend/src/components/screens/Home.js
@@ -7,20 +7,20 @@ import {SafeAreaView,
   ActivityIndicator,
  } from 'react-native';
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { NotFound, Perdidos, Achados } from "../../../assets";
+import { Perdidos, Achados } from "../../../assets";
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
 import MenuContainer from "../MenuContainer";
 
 import { FontAwesome } from "@expo/vector-icons";
-import ItemCarDontainer from "../ItemCardDontainer";
+import ItemCardContainer from "../ItemCardContainer";
 import { getPlacesData } from "../../../api";
 
 export default function Home(){
 
   const navigation = useNavigation();
 
-  const [type, setType] = useState("restaurants");
+  const [type, setType] = useState("perdidos");
   const [isLoading, setIsLoading] = useState(false);
   const [mainData, setMainData] = useState([]);
   const [bl_lat, setBl_lat] = useState(null);
@@ -64,28 +64,22 @@ export default function Home(){
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
             console.log(details?.geometry?.viewport);
-            setBl_lat(details?.geometry?.viewport?.southwest?.lat);
-            setBl_lng(details?.geometry?.viewport?.southwest?.lng);
-            setTr_lat(details?.geometry?.viewport?.northeast?.lat);
-            setTr_lng(details?.geometry?.viewport?.northeast?.lng);
           }}
           query={{
-            key: "YOUR_API_KEY",
-            language: "en",
+            key: "AIzaSyDWVvpx5KcTUZ6RGCYJWG7eFdVy7lJYMBM",
+            language: "pt-pt",
           }}
         />
       </View>
 
       {/* Menu Container */}
-      {isLoading ? (
         <View className=" flex-1 items-center justify-center">
           <ActivityIndicator size="large" color="#0B646B" />
         </View>
-      ) : (
         <ScrollView>
           <View className=" flex-row items-center justify-between px-8 mt-8">
             <MenuContainer
-              key={"hotels"}
+              key={"perdidos"}
               title="Perdidos"
               imageSrc={Perdidos}
               type={type}
@@ -95,7 +89,7 @@ export default function Home(){
            
 
             <MenuContainer
-              key={"restaurants"}
+              key={"achados"}
               title="Achados"
               imageSrc={Achados}
               type={type}
@@ -121,39 +115,15 @@ export default function Home(){
             </View>
 
             <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
-              {mainData?.length > 0 ? (
-                <>
-                  {mainData?.map((data, i) => (
-                    <ItemCarDontainer
-                      key={i}
-                      imageSrc={
-                        data?.photo?.images?.medium?.url
-                          ? data?.photo?.images?.medium?.url
-                          : "https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
-                      }
-                      title={data?.name}
-                      location={data?.location_string}
-                      data={data}
-                    />
-                  ))}
-                </>
-              ) : (
-                <>
-                  <View className="w-full h-[400px] items-center space-y-8 justify-center">
-                    <Image
-                      source={NotFound}
-                      className=" w-32 h-32 object-cover"
-                    />
-                    <Text className="text-2xl text-[#428288] font-semibold">
-                      Ups...Sem dados encontrado
-                    </Text>
-                  </View>
-                </>
-              )}
+             
+            <ItemCardContainer key={"101"} imageSrc={"https://cdn.pixabay.com/photo/2023/06/07/18/14/giraffes-8047856_1280.jpg"} title="something very biger for some people" location="Lisboa" />
+
+<ItemCardContainer key={"102"} imageSrc={"https://cdn.pixabay.com/photo/2014/12/22/10/04/lions-577104_1280.jpg"} title="sample" location="Leiria" />
+
+              
             </View>
           </View>
         </ScrollView>
-      )}
 
      </SafeAreaView>
   )
